3abdf3ec1bd7bf02c21cde397b40ee422400ca73
Stavros <snikoletopoulos@hotmail.com>
2022-03-25 19:06:46 +0200

    Create basic layout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,66 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	TextStyle,
+	ViewStyle,
+	View,
+	Button,
+	TextInput,
+} from "react-native";
 
-export default function App() {
+import { StatusBar } from "expo-status-bar";
+
+const App = () => {
 	return (
 		<View style={styles.container}>
 			<StatusBar style="auto" />
-			<Text>Test</Text>
+			<View style={styles.inputContainer}>
+				<TextInput style={styles.textInput} placeholder="Your course goal!" />
+				<Button title="Add goal" onPress={() => {}} />
+			</View>
+			<View style={styles.goalsContainer}>
+				<Text>List of goals...</Text>
+			</View>
 		</View>
 	);
+};
+
+export default App;
+
+interface Styles {
+	container: ViewStyle;
+	inputContainer: ViewStyle;
+	textInput: TextStyle;
+	goalsContainer: ViewStyle;
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create<Styles>({
 	container: {
 		flex: 1,
-		backgroundColor: "#fff",
+		paddingTop: 50,
+		paddingHorizontal: 16,
+	},
+
+	inputContainer: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "space-between",
 		alignItems: "center",
-		justifyContent: "center",
+		marginBottom: 24,
+		borderBottomWidth: 1,
+		borderBottomColor: "#ccc",
+	},
+
+	textInput: {
+		width: "70%",
+		borderColor: "black",
+		borderWidth: 1,
+		padding: 10,
+		marginBottom: 10,
+		marginRight: 8,
+	},
+
+	goalsContainer: {
+		flex: 5,
 	},
 });
