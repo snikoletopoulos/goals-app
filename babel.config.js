575c2daf77994a31cbae96cf8934e15039522e5d
Stavros <snikoletopoulos@hotmail.com>
2022-03-25 18:48:29 +0200

    Add default configuration files

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+// @ts-ignore
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [
+			[
+				"module-resolver",
+				{
+					alias: {
+						components: "./app/components",
+						assets: "./app/assets",
+						screens: "./app/screens",
+					},
+				},
+			],
+		],
+	};
 };
